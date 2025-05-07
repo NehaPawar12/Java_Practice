471458616d6df03a4d014e35c942ce8b1c7310bd
Neha Pawar <nehapawar2320@gmail.com>
2025-05-07 20:09:54 +0530

    Java basics, comments, variables

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello It's Neha!!");    //println is used to get the other part on the next line.
+        System.out.print("Hello It's Neha \n");     // \n is also used for next line.
+        System.out.println("I love Java");
+
+        // Single Line comments
+        /*
+        This
+        is a multi-line comment.
+         */
+    }
+}
---
+++ MyFirstProject/src/Variables.java
@@ -0,0 +1,28 @@
+public class Variables {
+
+    public static void main(String [] args){
+
+        // Camel case is used for variables.
+        //int age;  This is declaration
+        //age = 22;  This is initialization
+
+        int age = 22;
+        System.out.println("Age is: " + age);
+
+        double price = 23.12;
+        System.out.println("Price is: " + price);
+
+        char grade = 'A';
+        System.out.println("Grade is: " + grade);
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        System.out.println(isStudent);
+
+        String name = "Neha";   //Series of characters
+        String language = "Java";
+        System.out.println("My name is: " + name);  // This is string concatenation
+
+        System.out.println("I'm " + name + " my age is " + age + " and I love " + language);
+    }
+}
